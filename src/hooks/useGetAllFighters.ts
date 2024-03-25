6d14c38f416b6e4f74c559bff6fb7c4a751aa093
bendiklangseth <bendik.langseth@makingwaves.com>
2024-03-25 13:47:05 +0100

    Example on only using cached data

--- src/hooks/useGetAllFighters.ts
+++ src/hooks/useGetAllFighters.ts
@@ -1,10 +1,17 @@
 import { getAllFighters } from "@/api/supabaseDb"
 import { useQuery } from "@tanstack/react-query"
 
-const useGetAllFighters = () => {
+/* Eksempel på hvordan man skrur av refetching av data 
+*  Hvis shouldFetch = false så settes staleTime til Infinity
+*  og vi skrur av mulighet til å kjøre nytt query. Da brukes kun cached data. 
+*/
+
+const useGetAllFighters = (shouldFetch?: boolean) => {
     const { data, isLoading, isError } = useQuery({
         queryKey: ['fighters'],
         queryFn: () => getAllFighters(),
+        staleTime: shouldFetch ? 0 : Infinity,
+        enabled: shouldFetch !== undefined ? shouldFetch: undefined, // Setter enabled: true / false KUN dersom vi sender inn shouldFetch
     })
     return { fighters: data, isFightersLoading: isLoading, isFightersError: isError };
 }
